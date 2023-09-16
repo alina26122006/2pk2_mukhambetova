449f3c48ee62992745687b3bceb2522f9a7eea40
Алина Сериковна Мухамбетова <criperfair@gmail.com>
2023-09-16 07:18:39 +0300

    добавлена 3 папрактика

---
+++ PZ_03/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace PZ_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            {
+                Console.WriteLine("Введите расстояние в сантиметрах, на которое прыгнул ученик:");
+                int расстояние = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введите ступень ученика (1, 2, 3 или 4):");
+                int ступень = Convert.ToInt32(Console.ReadLine());
+
+                switch (ступень)
+                {
+                    case 1:
+                        if (расстояние >= 110)
+                        {
+                            Console.WriteLine("Ученик сдал норму ГТО по прыжкам на 1 ступени");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ученик не сдал норму ГТО по прыжкам на 1 ступени");
+                        }
+                        break;
+
+                    case 2:
+                        if (расстояние >= 125)
+                        {
+                            Console.WriteLine("Ученик сдал норму ГТО по прыжкам на 2 ступени");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ученик не сдал норму ГТО по прыжкам на 2 ступени");
+                        }
+                        break;
+
+                    case 3:
+                        if (расстояние >= 140)
+                        {
+                            Console.WriteLine("Ученик сдал норму ГТО по прыжкам на 3 ступени");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ученик не сдал норму ГТО по прыжкам на 3 ступени");
+                        }
+                        break;
+
+                    case 4:
+                        if (расстояние >= 150)
+                        {
+                            Console.WriteLine("Ученик сдал норму ГТО по прыжкам на 4 ступени");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ученик не сдал норму ГТО по прыжкам на 4 ступени");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Некорректный номер ступени");
+                        break;
+                }
+            }
+        }
+
+    }
+}
