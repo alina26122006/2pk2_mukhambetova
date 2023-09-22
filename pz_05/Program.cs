2d4a8c53a484bad64bbb142fcea12076e36a80f8
Алина Сериковна Мухамбетова <criperfair@gmail.com>
2023-09-22 23:07:59 +0300

    5 практика

---
+++ pz_05/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace pz_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int n = int.Parse(Console.ReadLine());
+            double v = 0;
+            int i = 1;
+            while (i <= n)
+            {
+                v += 1.0 / i;
+                i++;
+            }
+            Console.WriteLine("Ответ:" + v);
+            Console.ReadKey();
+        }
+    }
+}
