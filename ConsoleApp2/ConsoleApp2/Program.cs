6cc59212b8aab85daeb9c27b04f2db8c735031b9
Алина Сериковна Мухамбетова <criperfair@gmail.com>
2023-11-18 23:53:15 +0300

    добавлена 9 практика

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:"); string input = Console.ReadLine();
+            string output = ReplaceYears(input);
+            Console.WriteLine("Изменённая строка:");
+            Console.WriteLine(output);
+        }
+        static string ReplaceYears(string input)
+        {
+            string output = input;
+            for (int year = 1999; year >= 1000; year--)// Все года от 1999 до 1000            {
+                string yearString = year.ToString();// Изменяем год на строку
+            output = output.Replace(yearString, "****");// Заменяем все года на "****"            }
+            return output;
+        }
+    }
+}
---
+++ pz_09/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string maxString = string.Empty; // Переменная для хранения строки с максимальным количеством символов
+
+            Console.WriteLine("Введите строки. Для завершения введите 'stop':");
+
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if (input.ToLower() == "stop") // Проверяем, введено ли слово "stop"
+                    break;
+
+                if (input.Length > maxString.Length) // Если текущая строка длиннее предыдущей максимальной строки, обновляем maxString
+                    maxString = input;
+            }
+
+            Console.WriteLine("Строка максимальной длины: " + maxString);
+        }
+    }
+}
