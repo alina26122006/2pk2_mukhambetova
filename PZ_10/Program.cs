3576e870aee6d6217767b1f9122d424d26da1123
Алина Сериковна Мухамбетова <criperfair@gmail.com>
2023-11-22 21:17:19 +0300

    добавлена практика 10

---
+++ PZ_10/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TextFormatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Ввод текста с консоли
+        Console.WriteLine("Введите текст:");
+        string inputText = Console.ReadLine();
+
+        // Приводим текст к нормальному виду
+        string normalizedText = NormalizeText(inputText);
+
+        // Выводим результат
+        Console.WriteLine("Нормализованный текст:");
+        Console.WriteLine(normalizedText);
+    }
+
+    static string NormalizeText(string text)
+    {
+        // Разделяем текст на предложения
+        string[] sentences = text.Split('.', '!', '?');
+
+        // Обработка каждого предложения
+        for (int i = 0; i < sentences.Length; i++)
+        {
+            // Удаляем лишние пробелы в предложении
+            sentences[i] = sentences[i].Trim();
+
+            // Проверяем, чтобы предложение было не пустым
+            if (!string.IsNullOrEmpty(sentences[i]))
+            {
+                // Преобразуем первую букву предложения в заглавную
+                sentences[i] = char.ToUpper(sentences[i][0]) + sentences[i].Substring(1).ToLower();
+            }
+        }
+
+        // Соединяем предложения в одну строку с разделителями
+        string result = string.Join(". ", sentences);
+
+        return result;
+        }
+    }
+}
