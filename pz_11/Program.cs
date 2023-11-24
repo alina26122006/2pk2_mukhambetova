7c05058abd3bf6e6f9c4364256ef70e2396bc01f
Алина Сериковна Мухамбетова <criperfair@gmail.com>
2023-11-24 11:25:17 +0300

    добавлена 11 практика

---
+++ pz_11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a = 0;
+            double result = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write("Введите число для вычисления третьей степени: ");
+                a = double.Parse(Console.ReadLine());
+                PowerA3(a, out result);
+                Console.WriteLine($"{a} в третьей степени равно {result}");
+            }
+        }
+
+        static void PowerA3(double a, out double result)
+        {
+            result = Math.Pow(a, 3);
+        }
+    }
+}
+
+        
+    
+
