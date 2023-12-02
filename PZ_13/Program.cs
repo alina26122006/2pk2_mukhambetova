44a0f39fea0ab3b22fc8b6a080de208a7e738c2a
Алина Сериковна Мухамбетова <criperfair@gmail.com>
2023-12-02 19:38:58 +0300

    добавлена 14 практика

--- PZ_13/Program.cs
+++ PZ_13/Program.cs
@@ -8,74 +8,110 @@ namespace PZ_13
 {
     internal class Program
     {
-        static double ArithmeticalProgression(int n)
+        static void Main(string[] args)
         {
-            if (n == 1)
-            {
-                return 0.5;
-            }
-            else
-            {
-                return ArithmeticalProgression(n - 1) + 0.3;
-            }
-        }
+            Console.WriteLine("Задание 1:");
 
-        static double GeometricalProgression(int n)
-        {
-            if (n == 1)
-            {
-                return 14;
-            }
-            else
-            {
-                return GeometricalProgression(n - 1) * 2;
-            }
-        }
+            double a1 = 0.5;
+            double d = 0.3;
 
-        static void PrintNumbers(int A, int B)
+            Console.WriteLine("Введите номер члена прогрессии: ");
+            double n = double.Parse(Console.ReadLine());
+
+            double an = CalculateNthTerm(a1, d, n);
+            Console.WriteLine($"n-ый член прогрессии: {an}");
+        }
+            static double CalculateNthTerm(double a1, double d, double n)
+            {
+                if (n == 1)
+                {
+                    return a1;
+                }
+                else
+                {
+                    return CalculateNthTerm(a1, d, n - 1) + d;
+                }
+            }
+        static void Maiin()
         {
+            Console.WriteLine("Задание 2:");
+
+            Console.WriteLine("Введите номер элемента в геометрической прогрессии:");
+            int s = Convert.ToInt32(Console.ReadLine());
+            int N = 'n';
+            int result = CalculateGeometric(14, 4, N);
+            Console.WriteLine($"n-ый член геометрической прогрессии: {result}");
+        }
+            static int CalculateGeometric(int b1, int q, int n)
+            {
+                if (n == 1)
+                {
+                    return b1;
+                }
+                else
+                {
+                    return q * CalculateGeometric(b1, q, n - 1);
+                }
+            }
+        static void z()
+        {
+            Console.WriteLine("Задание 3:");
+            Console.WriteLine("Введите первое число A:");
+            int A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число B:");
+            int B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Все числа от A до B: ");
             if (A < B)
+            {
+                PrintNumbersAscending(A, B);
+            }
+            else
+            {
+                PrintNumbersDescending(A, B);
+            }
+
+        }
+
+            static void PrintNumbersAscending(int A, int B)
             {
                 if (A <= B)
                 {
                     Console.WriteLine(A);
-                    PrintNumbers(A + 1, B);
+                    PrintNumbersAscending(A + 1, B);
                 }
             }
-            else
+
+            static void PrintNumbersDescending(int A, int B)
             {
                 if (A >= B)
                 {
                     Console.WriteLine(A);
-                    PrintNumbers(A - 1, B);
+                    PrintNumbersDescending(A - 1, B);
                 }
             }
-        }
-
-        static void Main()
+        static void zz()
         {
-            int n;
-            double result;
+            Console.WriteLine("Задание 4:");
 
-            // Задание 1
-            n = 5; // Примерное значение n
-            result = ArithmeticalProgression(n);
-            Console.WriteLine($"n-й член арифметической прогрессии: {result}");
+            Console.WriteLine("Введите число a:");
+            int a = int.Parse(Console.ReadLine());
 
-            // Задание 2
-            n = 4; // Примерное значение n
-            result = GeometricalProgression(n);
-            Console.WriteLine($"n-й член геометрической прогрессии: {result}");
+            Console.WriteLine("Введите степень i:");
+            int i = int.Parse(Console.ReadLine());
 
-            // Задание 3
-            int A = 5; // Примерные значения A и B
-            int B = 10;
-            PrintNumbers(A, B);
+            int res = Power(a, i);
+            Console.WriteLine($"{a} в степени {i} равно {res}");
         }
-    }
-}
-
-
-    
- 
+            static int Power(int a, int i)
+            {
+                if (i == 0)
+                {
+                    return 1;
+                }
+                return a * Power(a, i - 1);
+            }
+        }
+ }
 
---
+++ PZ_14/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+
+            // Создаем файл с именем S.txt
+            using (StreamWriter sw = new StreamWriter(input + ".txt"))
+            {
+                Console.WriteLine("Введите текст для записи в файл, для завершения введите пустую строку:");
+                string line;
+
+                // Записываем текст построчно
+                while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+                {
+                    sw.WriteLine(line);
+                }
+
+                // Определяем длину самой большой строки
+                int maxLength = GetMaxLength(input + ".txt");
+
+                // Дописываем символ «*» до длины самой большой строки
+                sw.BaseStream.Seek(0, SeekOrigin.End);
+                foreach (string str in File.ReadLines(input + ".txt"))
+                {
+                    sw.WriteLine(str.PadRight(maxLength, '*'));
+                }
+            }
+
+            Console.WriteLine($"Файл {input}.txt успешно создан и заполнен.");
+        }
+
+        // Функция для нахождения длины самой большой строки
+        static int GetMaxLength(string filename)
+        {
+            int maxLength = 0;
+            foreach (string line in File.ReadLines(filename))
+            {
+                if (line.Length > maxLength)
+                {
+                    maxLength = line.Length;
+                }
+            }
+            return maxLength;
+        }
+    }
+}
