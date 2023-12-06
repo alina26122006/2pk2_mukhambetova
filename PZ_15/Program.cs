1a705a11ddfede352fde02e715a3451927869f7f
Алина Сериковна Мухамбетова <criperfair@gmail.com>
2023-12-06 10:11:56 +0300

    добавлена 15 практика

---
+++ PZ_15/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите полный путь к каталогу:");
+            string directoryPath = Console.ReadLine();
+
+            if (Directory.Exists(directoryPath))
+            {
+                string[] files = Directory.GetFiles(directoryPath);
+                foreach (string file in files)
+                {
+                    Console.WriteLine($"Вы хотите удалить файл {Path.GetFileName(file)}? Y или N");
+                    if (Console.ReadLine().ToUpper() == "Y")
+                    {
+                        File.Delete(file);
+                        Console.WriteLine($"Файл {Path.GetFileName(file)} удален");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Файл {Path.GetFileName(file)} оставлен без изменений");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Указанный каталог не существует");
+            }
+        }
+    }
+}
